13b9cbc3c01a8cdeb31fea9f93803f82b6f963fd
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-21 15:36:35 +0100

    add comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ console.log("hello");
 
 
 for (let i = 1; i <= 100; i++){
-    let divideThree = (i % 3);
-    let divideFive = (i % 5);
+    let divideThree = (i % 3); // questa variabile per verificare i multipli di 3
+    let divideFive = (i % 5); // questa variabile per verificare i multipli di 5
    
-    if (divideThree === 0 && divideFive === 0){
+    if (divideThree === 0 && divideFive === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 3 o 5
         console.log("Fizz-buz");
-    } else if(divideFive === 0){
+    } else if(divideFive === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 5
         console.log("buzz");
-    } else if( divideThree === 0){
+    } else if( divideThree === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 3
         console.log("fizz");
     } else{
         console.log(i);
