3debdf0527ae8482df8215376519e12ba13ceb9a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-21 15:34:55 +0100

    edit if fizz-buzz position in if function now works

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,12 @@ for (let i = 1; i <= 100; i++){
     let divideThree = (i % 3);
     let divideFive = (i % 5);
    
-    if( divideThree === 0){
-        console.log("fizz");
+    if (divideThree === 0 && divideFive === 0){
+        console.log("Fizz-buz");
     } else if(divideFive === 0){
         console.log("buzz");
-    } else if(divideThree === 0 && divideFive === 0){
-        console.log("Fizz-buz");
+    } else if( divideThree === 0){
+        console.log("fizz");
     } else{
         console.log(i);
     }
