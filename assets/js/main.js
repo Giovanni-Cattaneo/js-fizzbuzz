c1f85f5ca05e937875da3f9bc8ae56b3700a808f
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-21 16:03:29 +0100

    removed comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,11 @@ for (let i = 1; i <= 100; i++){
     let list = document.querySelector("ul.list")
 
     function myFunction(){
-        if (divideThree === 0 && divideFive === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 3 o 5
+        if (divideThree === 0 && divideFive === 0){ 
             list.append("Fizz-buz");
-        } else if(divideFive === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 5
+        } else if(divideFive === 0){ 
             list.append("buzz");
-        } else if( divideThree === 0){ // qui verifichiamo che non ci siano avanzi dall'operazione di i divisa per un numero multiplo di 3
+        } else if( divideThree === 0){ 
             list.append("fizz");
         } else{
             list.append(i)
