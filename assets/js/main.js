0f154604c5ea63ff38f31e30dae9163866a039d8
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-21 15:32:48 +0100

    add if in for

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,14 @@ console.log("hello");
 
 for (let i = 1; i <= 100; i++){
     let divideThree = (i % 3);
+    let divideFive = (i % 5);
    
     if( divideThree === 0){
         console.log("fizz");
+    } else if(divideFive === 0){
+        console.log("buzz");
+    } else if(divideThree + divideFive === 0){
+        console.log("Fizz-buz");
     } else{
         console.log(i);
     }
